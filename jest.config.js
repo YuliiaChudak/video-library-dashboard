6ed4bde76b9838c33f6fef3b57d4b053d351aed1
yuliiaChudak <lifespirit199392@gmail.com>
2025-07-21 22:39:35 +0200

    Updates path once breaking test CI is done

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 };
